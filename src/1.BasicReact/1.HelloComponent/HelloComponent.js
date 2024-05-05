f8b52f0e77055564e8410a05bb9a7df296d5824b
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-05-05 13:42:46 +0530

    Basic intro to React

---
+++ src/1.BasicReact/1.HelloComponent/HelloComponent.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function HelloComponent(props) {
+  //   const name = props.name;
+  //   const age = props.age;
+
+  //   Destructuring
+  const { name, age } = props;
+
+  console.log(props, "HelloComponent");
+
+  return (
+    <h1>
+      Hello component {name} {age}
+    </h1>
+  );
+}
+
+export default HelloComponent;
+
+// Props -> Are the for the component to get the extra info from outside
+// If we want to make our component re-usable we have to use props.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import HelloComponent from "./1.BasicReact/1.HelloComponent/HelloComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>App </h2>
+      <HelloComponent name={"utkarsh"} age={12} />
+      <HelloComponent name={"ankus"} age={22} />
+      <HelloComponent name={"amit"} age={32} />
+      <HelloComponent name={"sumit"} age={42} />
+      <HelloComponent name={"animesh"} age={52} />
     </div>
+
+    // MORE ABOUT JSX
+
+    // <div>
+    //   <p></p>
+
+    //   <h1></h1>
+    //   <div></div>
+    // </div>
+
+    //<div>abcd</div>  // tHis is illegal bec i already have a root node.
+
+    // BEHIND THE SCNESE
+
+    // JS ->
+    // document.createElement()
+
+    // REACT ->
+    // return React.createElement("div", {}, React.createElement("p"));
+    // return React.createElement ("div", {}, ""); // this is illegal
   );
 }
 
 export default App;
+
+/*
+
+  COMPONENT:  App is a component.
+
+  inside a component we can use a component 
+  inside index.js we can also use multiple components.
+
+
+
+   JSX -> Javascript XML.
+   It looks and feel like HTML but in reality its not HTML.
+
+   Behind the scences it is something different. -> (REact.createelement());
+
+  We cannot have 2 return from a function hence we cant have 2 Roots in JSX.
+
+
+
+*/
